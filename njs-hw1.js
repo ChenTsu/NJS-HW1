62f72fbb71851c9114b0d22b543dfcaa316a4f14
ChenTsu <chentsu@MSK-WSH-096.safetynet>
2016-10-19 18:19:18 +0300

    create directories with module mkdirp. use stdout for beep

--- njs-hw1.js
+++ njs-hw1.js
@@ -5,11 +5,36 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+// подключаем модуль для цветного вывода
+var clc = require('cli-color');
+
+// подключаем модуль создания вложенных директорий
+var mkdirp = require('mkdirp');
+
+// подключаем beep
+// var beep = require('beepbeep')
+
+// можно указать default/path/
 var path='';
 
-rl.question('Enter path for new directory ', (answer) => {
-    // TODO: Log the answer in a database
-    //console.log('Thank you for your valuable feedback:', answer);
-    path = answer;
+rl.question("Creating new directory.\nEnter path for new directory ", (answer) => {
+
+    if ( answer ) path = answer;
+
+    if ( path !== '')
+    {
+        mkdirp( path, function (err) {
+            if (err) console.error(err)
+            else console.log('Директория '+clc.green(path)+' успешно создана.')
+        });
+    }
+    else {
+        console.log(clc.yellow('Укажите путь для создания директории.'));
+        process.stdout.write('\x07');
+        // beep();
+    }
 rl.close();
 });
+
+
+
